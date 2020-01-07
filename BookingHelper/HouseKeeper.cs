f7db0b1ff717f7cf68f46519e2b2e6b3dbb9ffee
dburnat <d.burnat97@gmail.com>
2020-01-07 18:43:23 +0100

    Separated classes

---
+++ BookingHelper/HouseKeeper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingHelper
+{
+    public class Housekeeper
+    {
+        public string Email { get; set; }
+        public int Oid { get; set; }
+        public string FullName { get; set; }
+        public string StatementEmailBody { get; set; }
+    }
+}
