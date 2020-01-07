6b89479a2eb46f256896ba9f629c41e356e7a9c8
dburnat <d.burnat97@gmail.com>
2020-01-07 18:44:41 +0100

    Added 4 new tests

--- HouseKeeperHelperTests/HouseKeeperServiceTests.cs
+++ HouseKeeperHelperTests/HouseKeeperServiceTests.cs
@@ -10,7 +10,7 @@ namespace HouseKeeperHelperTests
     public class HouseKeeperServiceTests
     {
         private HousekeeperService _housekeeperService;
-        private HouseKeeper _houseKeeper;
+        private Housekeeper _houseKeeper;
         private Mock<IUnitOfWork> _unitOfWork;
         private Mock<IStatementGenerator> _statementGenerator;
         private Mock<IEmailSender> _emailSender;
@@ -19,36 +19,56 @@ namespace HouseKeeperHelperTests
         public void Setup()
         {
 
-            _house
+            _houseKeeper = new Housekeeper();
             _unitOfWork = new Mock<IUnitOfWork>();
             _statementGenerator = new Mock<IStatementGenerator>();
             _emailSender = new Mock<IEmailSender>();
             _messageBox = new Mock<IXtraMessageBox>();
-
             _housekeeperService = new HousekeeperService(
                 _unitOfWork.Object, _statementGenerator.Object, _emailSender.Object
                 , _messageBox.Object);
         }
-
-        [Test]
-        public void Test1()
-        {
-            _housekeeperService.SendStatementEmails(date);
-
-            _statementGenerator.Verify(d => d.SaveStatement());
-        }
-
-
-
         [Test]
         public void SendStatementsEmails_HouseKeepersEmailIsNull_ShouldNotGenerateStatement()
         {
-            _houseKeeper.email = null;
+            var date = DateTime.Now;
             _housekeeperService.SendStatementEmails(date);
+            _houseKeeper.Email = null;
 
             _statementGenerator.Verify(c => c.SaveStatement(
                 _houseKeeper.Oid, _houseKeeper.FullName, date), Times.Never);
         }
 
+        [Test]
+        public void SendStatementEmails__ShouldReturnTrue()
+        {
+            var date = DateTime.Now;
+            _houseKeeper.Email = "adam@example.com";
+             
+            _housekeeperService.SendStatementEmails(date);
+
+            Assert.AreEqual(true,_housekeeperService.SendStatementEmails(date));
+        }
+
+        [Test]
+        public void SendStatementEmails_HouseKeeperHasNoFullName_ShouldNotGenerateStatement()
+        {
+            var date = DateTime.Now;
+            _houseKeeper.FullName = "";
+
+            _housekeeperService.SendStatementEmails(date);
+            
+            _statementGenerator.Verify(c => c.SaveStatement
+                (_houseKeeper.Oid, _houseKeeper.FullName, date), Times.Never);
+        }
+
+        [Test]
+        public void SendStatementEmails_EverythingIsCorrect_ShouldReturnTrue()
+        {
+            var date = DateTime.Now;
+
+            Assert.AreEqual(true,_housekeeperService.SendStatementEmails(date));
+        }
+        
     }
 }
